65895e583088f15e68da7e3e2ae14942ee54d721
xiemy <4385223434@qq.com>
2020-10-01 22:32:16 +0800

    fix path

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 export { default as Button } from './Button';
 export { default as Tree } from './Tree/tree';
 export { default as Icon } from './Icon/icon';
-export { modal, Modal, Alert, Confirm } from './Modal/Modal';
-export { Layout, Content, Header, Footer, Aside } from './Layout/Layout';
-export { default as Spin } from './Spin/Spin';
-export { TabPane, Tab } from './Tab/Tab';
-export { default as Popover } from './Popover/Popover';
+export { modal, Modal, Alert, Confirm } from './Modal/modal';
+export { Layout, Content, Header, Footer, Aside } from './Layout/layout';
+export { default as Spin } from './Spin/spin';
+export { TabPane, Tab } from './Tab/tab';
+export { default as Popover } from './Popover/popover';
 export { default as ClickOutside } from './ClickOutside/clickOutside';
